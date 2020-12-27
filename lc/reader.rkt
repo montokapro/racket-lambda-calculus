ccace035d7b4dcdeaadef7f082d9821153f884d0
Stephen Webber <montokapro@gmail.com>
2020-12-27 12:14:49 -0600

    reader

--- lc/reader.rkt
+++ lc/reader.rkt
@@ -1,16 +1,55 @@
 #lang racket
+
 (require syntax/strip-context)
 
 (provide (rename-out [read-inner read]
                      [read-syntax-inner read-syntax]))
 
-(define (read-inner in)
-  (syntax->datum
-   (read-syntax-inner #f in)))
+(define (string->token s)
+  (let ([maybe-number (string->number s)])
+    (cond
+      ((number? maybe-number) maybe-number)
+      (#t (string->symbol s)))))
+
+(define (lexe in)
+  (map
+   string->token
+   (string-split (port->string in))))
+
+(define (expand-inner expand-acc)
+  (let ([token (car expand-acc)]
+        [remainder (cdr expand-acc)])
+    (match token
+      ['app
+       (letrec ([expand-acc-a (expand-inner remainder)]
+                [expr-a (car expand-acc-a)]
+                [remainder-a (cdr expand-acc-a)]
+                [expand-acc-b (expand-inner remainder-a)]
+                [expr-b (car expand-acc-b)]
+                [remainder-b (cdr expand-acc-b)])
+         (cons
+          `(app ,expr-a ,expr-b)
+          remainder-b))]
+      ['abs
+       (letrec ([expand-acc-a (expand-inner remainder)]
+                [expr-a (car expand-acc-a)]
+                [remainder-a (cdr expand-acc-a)])
+         (cons
+          `(abs ,expr-a)
+          remainder-a))]
+      [(var v)
+       expand-acc])))
+
+(define (expand tokens)
+  (car (expand-inner tokens)))
 
 (define (read-syntax-inner src in)
-  (with-syntax ([tokens (string-split (port->string in))])
+  (with-syntax ([expr (expand (lexe in))])
     (strip-context
      #'(module anything racket
          (provide data)
-         (define data 'tokens)))))
+         (define data 'expr)))))
+
+(define (read-inner in)
+  (syntax->datum
+   (read-syntax-inner #f in)))
